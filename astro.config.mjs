2fd830bc7be36305b67f82b22c6f71a3791e7e3e
Aden Pun <adenpun2000@gmail.com>
2023-06-15 14:21:58 +0800

    Redesigned

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: netlify(),
+  experimental: {
+    redirects: true,
+  },
+  integrations: [
+    svelte(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
+  output: "hybrid",
+  redirects: {
+    invite:
+      "https://discord.com/oauth2/authorize?client_id=995630008821354496&permissions=8&scope=bot+applications.commands",
+  },
+});
